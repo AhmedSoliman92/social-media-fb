d6460813619be7a2d486185f115baae25d5203b2
AhmedSoli92 <ahmed.soli101@gmail.com>
2022-05-04 06:55:47 +0200

    backend social media

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const connectDB = require('./config/DBConn');
+const userRouter = require('./routes/users')
+const authRouter = require('./routes/auth')
+const postRouter = require('./routes/post')
+const app = express();
+dotenv.config();
+// middleware
+app.use(express.json())
+app.use(helmet());
+app.use(morgan('common'));
+// connect to mogoDB
+connectDB()
+
+const PORT = process.env.PORT || 3500;
+
+app.use('/users',userRouter);
+app.use('/auth',authRouter);
+
+app.use('/posts',postRouter);
+
+
+mongoose.connection.once('open',()=>{
+    console.log("Connected to DB")
+    app.listen(PORT,()=>{
+        console.log(`Server running on port: ${PORT}` );
+    });
+    
+})
+
---
+++ backend/models/Users.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username:{
+        type: String,
+        min:3,
+        max:20,
+        required:true,
+        unique: true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required: true,
+        min:6,
+    },
+    profilePicture:{
+        type: String,
+        default:""
+    },
+    coverPicture:{
+        type:String,
+        default:""
+    },
+    followers:{
+        type: Array,
+        default:[]
+    },
+    following:{
+        type:Array,
+        default:[]
+    },
+    desc:{
+        type:String,
+        max:50
+    },
+    city:{
+        type:String,
+        max: 50
+    },
+    from:{
+        type:String,
+        max: 50
+    },
+    relationship: {
+        type: Number,
+        enum:[1,2,3]
+    },
+    isAdmin: {
+        type:Boolean,
+        default:false
+    },
+},
+{timestamps: true}
+);
+
+module.exports = mongoose.model("User",userSchema)
